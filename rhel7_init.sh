e11f4d33202f243bad465397054b4a3b52666bef
Ken Moini <ken@kenmoini.com>
2020-07-20 14:10:53 -0400

    whoops

--- rhel7_init.sh
+++ rhel7_init.sh
@@ -4,7 +4,7 @@
 subscription-manager repos --enable rhel-7-server-ansible-2.9-rpms
 
 ## Install git
-yum install -y git nano wget curl ansible
+yum install -y git nano wget curl ansible bridge-utils rhel-system-roles
 
 ## Install linux-system-roles network role
 ansible-galaxy install linux-system-roles.network
--- rhel8_init.sh
+++ rhel8_init.sh
@@ -4,7 +4,7 @@
 subscription-manager repos --enable ansible-2.9-for-rhel-8-x86_64-rpms
 
 ## Install git
-dnf install -y git nano wget curl ansible
+dnf install -y git nano wget curl ansible bridge-utils rhel-system-roles
 
 ## Install linux-system-roles network role
 ansible-galaxy install linux-system-roles.network
